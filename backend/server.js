114ecac73fd923e667184f4acbc980740e3677a0
MadelinePOBrien <mpietran@purdue.edu>
2022-10-10 23:26:56 -0500

    Made data and practice sending info

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const { chats } = require("./data/data");
 
+//Can use nodemon to auto update it, just change start: node to nodemon
 app.get('/', (req,res) => {
     res.send("API is running");
-})
+});
+
+app.get('/api/chat',(req,res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
 
 app.listen(5000, console.log("Server started at port 5000"));
+//Security issue that we use port 5000, we should use a variable or something
