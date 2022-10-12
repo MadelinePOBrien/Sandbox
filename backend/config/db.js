5eae987b617a0a317933b5bec5b26deabb3fb244
MadelinePOBrien <mpietran@purdue.edu>
2022-10-11 22:27:18 -0500

    installed dotenv, tried connecting to mongo

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+    try {
+        //Might have to go back and add dotenv and a env file, not sure
+        const conn = await mongoose.connect(process.env.MONGO_URI,  { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useFindAndModify: true, 
+         });
+    console.log(`MongoDB Connected: ${conn.connection.host})`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit();
+    }
+
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const { chats } = require("./data/data");
+
+dotenv.config();
 
 //Can use nodemon to auto update it, just change start: node to nodemon
 app.get('/', (req,res) => {
@@ -16,5 +19,7 @@ app.get('/api/chat/:id', (req, res) => {
     res.send(singleChat);
 });
 
+const PORT = process.env.PORT || 5000
+//Might have to replace 50000 with port below
 app.listen(5000, console.log("Server started at port 5000"));
 //Security issue that we use port 5000, we should use a variable or something
